2c8864f129fc1e153f20bce99d402f6d8e1b37cd
Avi Dalal <avdalal@deloitte.co.il>
2023-09-24 16:12:43 +0300

    excel reader

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,19 +1,57 @@
-import * as React from 'react'
+import React, { useState, useRef, ChangeEvent } from 'react';
 import { createRoot } from 'react-dom/client';
-import './options.css'
+import './options.css';
+import readXlsxFile, { Row } from 'read-excel-file';
 
-
-
-const App: React.FC<{}> = () => {
-  return (
-    <div>
-      <img src="icon.png" />
-    </div>
-  )
+interface PageData {
+  pageurl: string;
+  pageTemplate: string;
 }
 
-const root = document.createElement('div')
-document.body.appendChild(root)
+const App: React.FC<{}> = () => {
+  const inputEl = useRef<HTMLInputElement>(null);
+  const [pageData, setPageData] = useState<PageData[]>([]);
 
-const reactRoot = createRoot(root)
-reactRoot.render(<App />)
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const input = event.target;
+    if (input?.files) {
+      readXlsxFile(input.files[0]).then((rows: Row[]) => {
+        const [, ...data] = rows;
+
+        const pageData = data.map((row) => ({
+          pageurl: row[0] as string,
+          pageTemplate: row[1] as string,
+        }));
+
+        setPageData(pageData);
+      });
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        id="input"
+        ref={inputEl}
+        onChange={onChangeHandler}
+      />
+      <div>
+        <h2>Extracted Data:</h2>
+        <ul>
+          {pageData.map((item, index) => (
+            <li key={index}>
+              Page URL: {item.pageurl}, Page Template: {item.pageTemplate}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+const reactRoot = createRoot(root);
+reactRoot.render(<App />);
