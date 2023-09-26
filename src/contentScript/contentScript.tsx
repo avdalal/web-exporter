2e9883b4ae441ee7f9a8f9fc2f99e4db56488e11
Avi Dalal <avdalal@deloitte.co.il>
2023-09-26 15:38:42 +0300

    add contentexportchild

--- src/contentScript/contentScript.tsx
+++ src/contentScript/contentScript.tsx
@@ -5,39 +5,67 @@ const componentNames = [
     "rich-text-component"
 ];
 
+const addComName = ( comp: Element, element: string, type: string) => {
+    if(type == "parent") {
+        comp.classList.add("component");
+    } 
+    if(type === "child") {
+        comp.classList.add("child-component");
+    }
+    // Check if a .par-div already exists
+    if (!comp.querySelector('.par-div')) {
+        const pardiv = document.createElement("div");
+        pardiv.className = "par-div";
+        const name = document.createElement("span");
+        name.innerHTML = `${element}`;
+        name.className = "component-name";
+        pardiv.prepend(name); 
+        comp.prepend(pardiv);
+    }
+}
+
 componentNames.forEach(element => {
     const components = document.getElementsByClassName(element);
     Array.from(components).forEach(comp => {
-        addComName(comp, element) ;
+        addComName(comp, element, "parent") ;
     });
 });
 
 const elementsWithContentExport = document.querySelectorAll('[contentexport]');
 const contentExportValues = [];
+const childelementsWithContentExport = document.querySelectorAll('[contentexportchild]');
+const childcontentExportValues = [];
 
 elementsWithContentExport.forEach(function(element) {
     const attributeValue = element.getAttribute('contentexport');
     contentExportValues.push(attributeValue);
 });
 
+childelementsWithContentExport.forEach(function(element) {
+    const attributeValue = element.getAttribute('contentexportchild');
+    childcontentExportValues.push(attributeValue);
+});
+
 contentExportValues.forEach(element => {
     const components = document.querySelectorAll(`[contentexport=${element}]`);
 
     Array.from(components).forEach(comp => {
-        addComName(comp, element) 
+        if (element == "mainText") {
+            // const htmlContent = comp.innerHTML;
+            // let modifiedContent = htmlContent.replace(/<h2 /gi, "[SPLIT]<h2 ").replace(/<\/h2>/gi, "</h2>[SPLIT]");
+            // const sections = modifiedContent.split("[SPLIT]");
+           // console.log(sections);
+        }
+        addComName(comp, element, "parent");
+    });
+});
+childcontentExportValues.forEach(element => {
+    const components = document.querySelectorAll(`[contentexportchild=${element}]`);
+
+    Array.from(components).forEach(comp => {
+        addComName(comp, element, "child");
     });
 });
 
-const addComName = ( comp: Element, element: string) => {
-        comp.classList.add("component");
-        // Check if a .par-div already exists
-        if (!comp.querySelector('.par-div')) {
-            const pardiv = document.createElement("div");
-            pardiv.className = "par-div";
-            const name = document.createElement("span");
-            name.innerHTML = `${element}`;
-            name.className = "component-name";
-            pardiv.prepend(name); 
-            comp.prepend(pardiv);
-        }
-}
+
+
--- src/utils/page.ts
+++ src/utils/page.ts
@@ -1,3 +1,10 @@
+import readXlsxFile from 'read-excel-file'
+import { By, WebElement } from 'selenium-webdriver'; // Import necessary WebDriver modules
+
+import * as fs from 'fs';
+import * as path from 'path';
+import * as https from 'https';
+
 type Obj = {[key: string] : string | Object}
 const page: Obj = {};
 const _ComponentLis: Obj = {};
@@ -8,4 +15,124 @@ _ComponentLis.TableWithBlueHeading_0_2 = TableWithBlueHeading_0_2;
 page._ComponentLis = _ComponentLis;
 
 
-console.log(page);
+
+const workingDirectory: string = process.env.HOME || process.env.USERPROFILE || '';
+const site = "888poker";
+const damBase = "/content/dam/holdings888/888poker/";
+const lang = "com/es/";
+
+
+function findWebElement(element: WebElement, findBy: By): WebElement | null {
+    const elem = element.findElements(findBy);
+    return elem.length > 0 ? elem[0] : null;
+}
+
+
+
+function getLastBitFromUrl(url: string): string {
+    return url.replace(/.*\/([^/?]+).*/, "$1");
+}
+
+
+function getAltFromUrl(url: string): string {
+    let alt: string = getLastBitFromUrl(url);
+    alt = alt.substring(0, alt.lastIndexOf("."));
+    return alt;
+}
+
+
+function CreateFolder(dirName: string): string {
+    try {
+        if (!fs.existsSync(dirName)){
+            fs.mkdirSync(dirName, { recursive: true });
+        }
+        return dirName;
+    } catch (error) {
+        console.log(error.toString());
+        return '';
+    }
+}
+
+function DownloadMediaByURL(imagepath: string, fileUrl: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        try {
+            const mediaUrl = new URL(fileUrl.startsWith('https:') ? fileUrl : `https:${fileUrl}`);
+            const fileName = imagepath.replace(/%20/g, '_').replace(/%/g, '_');
+            const pathToSave = path.join(workingDirectory, site, imagepath, fileName);
+
+            // Create folder if not exists
+            CreateFolder(path.join(workingDirectory, site, imagepath));
+
+            const fileStream = fs.createWriteStream(pathToSave);
+
+            https.get(mediaUrl, (response) => {
+                response.pipe(fileStream);
+
+                fileStream.on('finish', () => {
+                    fileStream.close();
+                    resolve(path.join(damBase, lang, imagepath, fileName));
+                });
+            }).on('error', (error) => {
+                fs.unlink(pathToSave, () => {}); // Delete the file if an error occurs
+                reject(error);
+            });
+        } catch (error) {
+            console.log(error.toString());
+            reject(error);
+        }
+    });
+}
+
+
+function getImageUrlFromDom(element: WebElement): string | null {
+    let imageElement: string | null;
+    imageElement = element.getAttribute("src") !== null ? element.getAttribute("src") : element.getAttribute("href");
+    if (imageElement !== null && imageElement.includes("data:image")) {
+        imageElement = element.getAttribute("data-original");
+    }
+    return imageElement;
+}
+
+
+async function map(path: string, _pageElement): Promise<Obj | null> {
+    const image : Obj = {}
+
+    try {
+
+        let imageElement = await _pageElement.getCssValue("background-image");
+
+        if (imageElement.includes("none")) {
+            const imageElements: WebElement[] = await _pageElement.findElements(By.tagName("img"));
+            if (imageElements.length === 0) {
+                imageElement = getImageUrlFromDom(_pageElement);
+            }
+            for (const webElement of imageElements) {
+                if (imageElement.length === 0 || imageElement.includes("none")) {
+                    imageElement = getImageUrlFromDom(webElement);
+                }
+            }
+        } else {
+            imageElement = imageElement.split("\"")[1];
+        }
+
+        image.imageSize = "100%";
+        image.fileReference = await DownloadMediaByURL(path, imageElement);
+        image.altValueFromDAM = false;
+        image.enableCaption = true;
+        image.enableModal = true;
+        image.paddingBottom = "None";
+        image.paddingTop = "None";
+        image.alt = (_pageElement.getAttribute("alt") === null || _pageElement.getAttribute("alt") === "")
+            ? getAltFromUrl(image.fileReference.toString())
+            : await _pageElement.getAttribute("alt");
+        image.caption = await findWebElement(_pageElement, By.className("image-teaser")) === null
+            ? ""
+            : await _pageElement.findElement(By.className("image-teaser")).getText();
+
+
+        return image;
+    } catch (e) {
+       console.log(`[ImageMapper] - ${e.toString()}`);
+    }
+    return null;
+}
